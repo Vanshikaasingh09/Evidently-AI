d54e56df8e1f65cf272ed375ff1f271967cedce2
username <160256273+Vanshikaasingh09@users.noreply.github.com>
2025-04-29 06:46:45 +0530

    ok

--- streamlit-app/app.py
+++ streamlit-app/app.py
@@ -16,7 +16,8 @@ from src.utils import EntityNotFoundError
 from src.utils import get_reports_mapping
 from src.utils import list_periods
 
-PROJECTS_DIR: Path = Path("./projects")
+PROJECTS_DIR: Path = Path(__file__).parent / "projects"
+
 REPORTS_DIR_NAME: Text = "reports"
 
 
